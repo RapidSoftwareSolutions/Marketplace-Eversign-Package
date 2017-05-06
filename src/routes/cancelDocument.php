e8eac656b4e889d8a80809791d25252f8368b53d
ustal <quit.ipad2@gmail.com>
2017-05-06 10:46:17 +0300

    fix cancelDocument

--- src/routes/cancelDocument.php
+++ src/routes/cancelDocument.php
@@ -7,7 +7,7 @@ $app->post('/api/Eversign/cancelDocument', function ($request, $response) {
 
     $settings = $this->settings;
     $checkRequest = $this->validation;
-    $validateRes = $checkRequest->validate($request, ['accessKey', 'businessId', 'documentHash', 'cancel']);
+    $validateRes = $checkRequest->validate($request, ['accessKey', 'businessId', 'documentHash']);
     if (!empty($validateRes) && isset($validateRes['callback']) && $validateRes['callback'] == 'error') {
         return $response->withHeader('Content-type', 'application/json')->withStatus(200)->withJson($validateRes);
     } else {
